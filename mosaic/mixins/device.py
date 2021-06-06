a0d0ec3f1c446795577f5a7484be091478f1ef23
Karan Goel <krandiash@Karans-iMac.attlocal.net>
2021-06-06 00:00:28 -0700

    lint

--- mosaic/mixins/device.py
+++ mosaic/mixins/device.py
@@ -4,23 +4,24 @@ import torch
 
 
 class DeviceMixin:
-
     def __init__(
-            self,
-            device: Union[int, str],
-            *args,
-            **kwargs,
+        self,
+        device: Union[int, str],
+        *args,
+        **kwargs,
     ):
         super(DeviceMixin, self).__init__(*args, **kwargs)
 
         if isinstance(device, str):
-            if device.startswith('cuda'):
-                self._cuda_device = 0 if ":" not in device else int(
-                    device.split(":")[-1])
-            elif device.startswith('gpu'):
-                self._cuda_device = 0 if ":" not in device else int(
-                    device.split(":")[-1])
-            elif device.startswith('cpu'):
+            if device.startswith("cuda"):
+                self._cuda_device = (
+                    0 if ":" not in device else int(device.split(":")[-1])
+                )
+            elif device.startswith("gpu"):
+                self._cuda_device = (
+                    0 if ":" not in device else int(device.split(":")[-1])
+                )
+            elif device.startswith("cpu"):
                 self._cuda_device = None
         elif isinstance(device, int):
             assert device >= 0, "`device` must be >= 0."
@@ -30,11 +31,11 @@ class DeviceMixin:
             self._device = "cuda" if torch.cuda.is_available() else "cpu"
 
         if self._cuda_device == -1:
-            self._device = 'cuda'
+            self._device = "cuda"
         elif self._cuda_device >= 0:
-            self._device = f'cuda:{self._cuda_device}'
+            self._device = f"cuda:{self._cuda_device}"
         else:
-            self._device = 'cpu'
+            self._device = "cpu"
 
     @property
     def cuda_device(self):
