8c25eebadb539c0c34f2be119aff6fd03b72c180
Sabri Eyuboglu <eyuboglu@stanford.edu>
2021-06-19 10:39:29 -0700

    Fix gaze url

--- mosaic/contrib/siim_cxr.py
+++ mosaic/contrib/siim_cxr.py
@@ -9,7 +9,7 @@ from PIL import Image
 
 from mosaic.cells.volume import MedicalVolumeCell
 
-GAZE_DATA_URL = "https://raw.githubusercontent.com/robustness-gym/mosaic/cxr-gaze/examples/03-med_img/cxr_gaze_data.json"  # noqa: E501
+GAZE_DATA_URL = "https://raw.githubusercontent.com/robustness-gym/mosaic/dev/examples/03-med_img/cxr_gaze_data.json"  # noqa: E501
 
 
 def download_siim_cxr(
