3b17f36457f1c34896d4c1e61786017ca9e6cb55
Sabri Eyuboglu <eyuboglu@stanford.edu>
2021-07-04 22:01:38 +0000

    Update GCSColumn

--- meerkat/contrib/mimic/__init__.py
+++ meerkat/contrib/mimic/__init__.py
@@ -2,62 +2,17 @@ from __future__ import annotations
 
 import io
 import os
+import subprocess
 from typing import Sequence
 
 import google.auth
+import numpy as np
 import pandas as pd
 from google.cloud import bigquery, bigquery_storage, storage
 from PIL import Image
+from pydicom.filereader import dcmread
 
 import meerkat as mk
-
-MODULES = ["cxr", "core", "hosp", "icu", "ed"]
-
-DICOM_FIELDS = [
-    "dicom",
-    "StudyDate",
-    "ImageType",
-    "TableType",
-    "DistanceSourceToDetector",
-    "DistanceSourceToPatient",
-    "Exposure",
-    "ExposureTime",
-    "XRayTubeCurrent",
-    "FieldOfViewRotation",
-    "FieldOfViewOrigin",
-    "FieldOfViewHorizontalFlip",
-    "ViewPosition",
-    "PatientOrientation",
-    "BurnedInAnnotation",
-    "RequestingService",
-    "DetectorPrimaryAngle",
-    "DetectorElementPhysicalSize",
-]
-
-
-class GCSLoader:
-    def __init__(
-        self,
-        bucket_name: str,
-        project: str,
-        loader: callable = None,
-        dataset_dir: str = None,
-        writer: callable = None,
-    ):
-        storage_client = storage.Client(project=project)
-        self.bucket = storage_client.bucket(bucket_name, user_project=project)
-        self.loader = (lambda x: x) if loader is None else loader
-        self.dataset_dir = dataset_dir
-        self.writer = writer
-
-    def __call__(self, blob_name):
-        out = self.loader(
-            io.BytesIO(self.bucket.blob(str(blob_name)).download_as_bytes())
-        )
-        if self.writer is not None:
-            path = os.path.join(self.dataset_dir, str(blob_name))
-            os.makedirs(os.path.dirname(path), exist_ok=True)
-            self.writer(path, out)
 
 
 class GCSImageColumn(mk.ImageColumn):
@@ -82,18 +37,20 @@ class GCSImageColumn(mk.ImageColumn):
 
         self.bucket = storage_client.bucket(bucket_name, user_project=project)
         self.loader = (lambda x: x) if loader is None else loader
+        self.writer = writer
         self.local_dir = local_dir
 
     def _get_cell(self, index: int, materialize: bool = True):
         if materialize:
             blob_name = self._data.iloc[index]
-
             if self.local_dir is not None and os.path.exists(
                 os.path.join(self.local_dir, str(blob_name))
             ):
                 # fetch locally if it's been cached locally
                 cell = mk.ImagePath(
-                    self._data.iloc[index], loader=self.loader, transform=self.transform
+                    os.path.join(self.local_dir, str(blob_name)),
+                    loader=self.loader,
+                    transform=self.transform,
                 )
                 return cell.get()
 
@@ -111,14 +68,35 @@ class GCSImageColumn(mk.ImageColumn):
 
         else:
             cell = mk.ImagePath(
-                self._data.iloc[index], loader=self.loader, transform=self.transform
+                os.path.join(self.local_dir, str(blob_name)),
+                loader=self.loader,
+                transform=self.transform,
             )
             return cell
+
+    def _get_batch(self, indices: np.ndarray, materialize: bool = True):
+        if materialize:
+            # if materializing, return a batch (by default, a list of objects returned
+            # by `.get`, otherwise the batch format specified by `self.collate`)
+            return self.collate(
+                [self._get_cell(idx, materialize=materialize) for idx in indices]
+            )
+
+        else:
+            data = self._data.iloc[indices].reset_index(drop=True)
+            return self.__class__(
+                filepaths=data,
+                loader=self.loader,
+                transform=self.transform,
+                bucket_name=self.bucket_name,
+                project=self.project,
+                local_dir=self.local_dir,
+            )
 
     @classmethod
     def _state_keys(cls) -> set:
         """List of attributes that describe the state of the object."""
-        return super()._state_keys() | {"bucket_name", "project"}
+        return super()._state_keys() | {"bucket_name", "project", "local_dir"}
 
     @staticmethod
     def concat(columns: Sequence[GCSImageColumn]):
@@ -131,52 +109,100 @@ class GCSImageColumn(mk.ImageColumn):
             filepaths=pd.concat([c.data for c in columns]),
             loader=loader,
             transform=transform,
-            bucket_name=bucket_name,
-            project=self.project,
+            bucket_name=columns[0].bucket_name,
+            project=columns[0].project,
+            local_dir=columns[0].local_dir,
         )
+
+
+def query_mimic(query_str: str, gcp_project: str) -> pd.DataFrame:
+    credentials, _ = google.auth.default(
+        scopes=["https://www.googleapis.com/auth/cloud-platform"]
+    )
+
+    # Make clients.
+    bqclient = bigquery.Client(
+        credentials=credentials,
+        project=gcp_project,
+    )
+    bqstorageclient = bigquery_storage.BigQueryReadClient(credentials=credentials)
+
+    df = (
+        bqclient.query(query_str)
+        .result()
+        .to_dataframe(bqstorage_client=bqstorageclient)
+    )
+
+    return df
+
+
+# we only include a subset of the fields in the dicom metadata
+DICOM_FIELDS = [
+    "dicom",
+    "StudyDate",
+    "ImageType",
+    "TableType",
+    "DistanceSourceToDetector",
+    "DistanceSourceToPatient",
+    "Exposure",
+    "ExposureTime",
+    "XRayTubeCurrent",
+    "FieldOfViewRotation",
+    "FieldOfViewOrigin",
+    "FieldOfViewHorizontalFlip",
+    "ViewPosition",
+    "PatientOrientation",
+    "BurnedInAnnotation",
+    "RequestingService",
+    "DetectorPrimaryAngle",
+    "DetectorElementPhysicalSize",
+]
 
 
 def build_mimic_dp(
     dataset_dir: str,
     gcp_project: str,
+    include_cxr_jpg: bool = True,
+    include_cxr_dicom: bool = True,
     include_labels: bool = True,
     include_dicom_meta: bool = True,
     include_patient: bool = True,
     include_admit: bool = True,
+    include_split: bool = True,
 ):
     os.environ["GOOGLE_CLOUD_PROJECT"] = gcp_project
 
-    query_str = f"""
+    query_str = """
             SELECT *
             FROM `physionet-data.mimic_cxr.record_list` cxr_records
             LEFT JOIN `physionet-data.mimic_cxr.study_list` cxr_studies
-                ON cxr_records.study_id = cxr_studies.study_id 
+                ON cxr_records.study_id = cxr_studies.study_id
     """
 
     if include_labels:
         query_str += """
             LEFT JOIN `physionet-data.mimic_cxr.chexpert` chexpert
-                ON cxr_records.study_id = chexpert.study_id 
+                ON cxr_records.study_id = chexpert.study_id
         """
 
     if include_patient:
         query_str += """
             LEFT JOIN `physionet-data.mimic_core.patients` patients
-                ON cxr_records.subject_id = patients.subject_id 
+                ON cxr_records.subject_id = patients.subject_id
         """
 
     if include_dicom_meta:
         query_str += f"""
             LEFT JOIN (SELECT {','.join(DICOM_FIELDS)}
-                FROM `physionet-data.mimic_cxr.dicom_metadata_string`) AS meta 
-            ON cxr_records.dicom_id = meta.dicom 
+                FROM `physionet-data.mimic_cxr.dicom_metadata_string`) AS meta
+            ON cxr_records.dicom_id = meta.dicom
         """
     elif include_admit:
         # need the StudyDate to include admission data
-        query_str += f"""
+        query_str += """
             LEFT JOIN (SELECT StudyDate
-                FROM `physionet-data.mimic_cxr.dicom_metadata_string`) AS meta 
-            ON cxr_records.dicom_id = meta.dicom 
+                FROM `physionet-data.mimic_cxr.dicom_metadata_string`) AS meta
+            ON cxr_records.dicom_id = meta.dicom
         """
 
     df = query_mimic(query_str, gcp_project=gcp_project)
@@ -205,37 +231,41 @@ def build_mimic_dp(
             admit_df.drop_duplicates(subset="study_id"), how="left", on="study_id"
         )
 
-    loader = GCSLoader(
-        bucket_name="mimic-cxr-jpg-2.0.0.physionet.org",
-        project=gcp_project,
-        loader=Image.open,
-        writer=lambda x, y: y.save(x),
-        dataset_dir=dataset_dir,
-    )
-
     dp = mk.DataPanel.from_pandas(df)
-    paths = pd.Series(dp["path"].data)
-    dp["jpg_path"] = paths.str.split(".").str[0] + ".jpg"
-    dp["img"] = mk.ImageColumn.from_filepaths(dp["jpg_path"], loader=loader)
+    if include_cxr_jpg:
+        paths = pd.Series(dp["path"].data)
+        dp["jpg_path"] = paths.str.split(".").str[0] + ".jpg"
+        dp["img"] = GCSImageColumn.from_filepaths(
+            filepaths=dp["jpg_path"],
+            bucket_name="mimic-cxr-jpg-2.0.0.physionet.org",
+            project=gcp_project,
+            loader=Image.open,
+            local_dir=dataset_dir,
+        )
+
+    if include_cxr_dicom:
+        dp["dicom"] = GCSImageColumn.from_filepaths(
+            filepaths=dp["path"],
+            bucket_name="mimic-cxr-2.0.0.physionet.org",
+            project=gcp_project,
+            loader=dcmread,
+            local_dir=dataset_dir,
+        )
+
+    if include_split:
+        storage_client = storage.Client(project=gcp_project)
+        bucket = storage_client.bucket(
+            "mimic-cxr-jpg-2.0.0.physionet.org", user_project=gcp_project
+        )
+        filepath = os.path.join(dataset_dir, "mimic-cxr-2.0.0-split.csv.gz")
+        bucket.blob("mimic-cxr-2.0.0-split.csv.gz").download_to_filename(filepath)
+        subprocess.run(["gunzip", filepath])
+
+        dp = dp.merge(
+            mk.DataPanel.from_csv(
+                os.path.join(dataset_dir, "mimic-cxr-2.0.0-split.csv")
+            )[["split", "dicom_id"]],
+            how="left",
+            on="dicom_id",
+        )
     return dp
-
-
-def query_mimic(query_str: str, gcp_project: str) -> pd.DataFrame:
-    credentials, _ = google.auth.default(
-        scopes=["https://www.googleapis.com/auth/cloud-platform"]
-    )
-
-    # Make clients.
-    bqclient = bigquery.Client(
-        credentials=credentials,
-        project=gcp_project,
-    )
-    bqstorageclient = bigquery_storage.BigQueryReadClient(credentials=credentials)
-
-    df = (
-        bqclient.query(query_str)
-        .result()
-        .to_dataframe(bqstorage_client=bqstorageclient)
-    )
-
-    return df
