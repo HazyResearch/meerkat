0078114783ea000b6d2f547a011ad56675657b33
Priya <priyamis@cse.iitk.ac.in>
2021-07-20 22:51:07 +0530

    InstancesColumn for object detection outputs; include instance_segmentation task for TensorModel

---
+++ meerkat/columns/instances_column.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+
+import itertools
+from typing import Sequence
+
+from torchvision.ops import nms
+from tqdm import tqdm
+
+from meerkat.columns.list_column import ListColumn
+
+
+class InstancesColumn(ListColumn):
+    def __init__(self, data: Sequence = None, *args, **kwargs):
+
+        super(InstancesColumn, self).__init__(data=data, *args, **kwargs)
+
+    def num_instances(
+        self, batch_size: int = 32
+    ) -> ListColumn:  # TODO(Priya): ListColumn or some other type
+        # Returns a column with the number of instances in each Instances object
+
+        data = []  # Holds the number of instances in each object
+        for batch in tqdm(
+            self.batch(batch_size),
+            total=(len(self) // batch_size + int(len(self) % batch_size != 0)),
+        ):
+            batch_data = [len(instance) for instance in batch]
+            data = list(itertools.chain(data, batch_data))
+
+        data_col = ListColumn(data)
+
+        return data_col
+
+    def get_field(self, field: str, batch_size: int = 32) -> ListColumn:
+        # Returns a column of the required field from each Instances object
+
+        data = []
+        for batch in tqdm(
+            self.batch(batch_size),
+            total=(len(self) // batch_size + int(len(self) % batch_size != 0)),
+        ):
+            batch_data = [instance.get_fields()[field] for instance in batch]
+            data = list(itertools.chain(data, batch_data))
+
+        data_col = ListColumn(data)
+
+        return data_col
+
+    def get_nms(
+        self,
+        iou_threshold: float,
+        batch_size: int = 32,
+    ) -> ListColumn:
+        # Returns ListColumn of tensors of indices given by torchvision.ops.nms
+
+        data = []
+        for batch in tqdm(
+            self.batch(batch_size),
+            total=(len(self) // batch_size + int(len(self) % batch_size != 0)),
+        ):
+            batch_data = [
+                nms(
+                    instance.get_fields()["pred_boxes"].tensor,
+                    instance.get_fields()["scores"],
+                    iou_threshold,
+                )
+                for instance in batch
+            ]
+            data = list(itertools.chain(data, batch_data))
+
+        data_col = ListColumn(data)
+        return data_col
