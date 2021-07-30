de091b86a44c80214a31181a0c3de37928fa4349
Sabri Eyuboglu <eyuboglu@stanford.edu>
2021-07-30 15:15:17 -0400

    Fix GCSColumn

--- meerkat/contrib/mimic/gcs.py
+++ meerkat/contrib/mimic/gcs.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import io
+from meerkat.columns.pandas_column import PandasSeriesColumn
 import os
 import subprocess
 from typing import Sequence
@@ -15,9 +16,9 @@ from meerkat import ImageColumn
 class GCSImageColumn(ImageColumn):
     def __init__(
         self,
+        blob_names: PandasSeriesColumn = None,
         bucket_name: str = None,
         project: str = None,
-        filepaths: Sequence[str] = None,
         transform: callable = None,
         loader: callable = None,
         writer: callable = None,
@@ -26,10 +27,11 @@ class GCSImageColumn(ImageColumn):
         **kwargs,
     ):
         super(GCSImageColumn, self).__init__(
-            filepaths, transform, loader, *args, **kwargs
+            blob_names, transform, loader, *args, **kwargs
         )
         self.project = project
         self.bucket_name = bucket_name
+        self._set_state()
         storage_client = storage.Client(project=project)
 
         self.bucket = storage_client.bucket(bucket_name, user_project=project)
@@ -37,19 +39,14 @@ class GCSImageColumn(ImageColumn):
         self.writer = writer
         self.local_dir = local_dir
 
-    def fn(self, filepath: str):
-
-        blob_name = self._data.iloc[index]
+    def fn(self, blob_name: str):
         if self.local_dir is not None and os.path.exists(
             os.path.join(self.local_dir, str(blob_name))
         ):
             # fetch locally if it's been cached locally
-            cell = mk.ImagePath(
-                os.path.join(self.local_dir, str(blob_name)),
-                loader=self.loader,
-                transform=self.transform,
+            return super(GCSImageColumn, self).fn(
+                os.path.join(self.local_dir, str(blob_name))
             )
-            return cell.get()
 
         # otherwise pull from GCP
         out = self.loader(
@@ -63,42 +60,34 @@ class GCSImageColumn(ImageColumn):
             self.writer(path, out)
         return out
 
-    def _get_batch(self, indices: np.ndarray, materialize: bool = True):
-        if materialize:
-            # if materializing, return a batch (by default, a list of objects returned
-            # by `.get`, otherwise the batch format specified by `self.collate`)
-            return self.collate(
-                [self._get_cell(idx, materialize=materialize) for idx in indices]
-            )
-
-        else:
-            data = self._data.iloc[indices].reset_index(drop=True)
-            return self.__class__(
-                filepaths=data,
-                loader=self.loader,
-                transform=self.transform,
-                bucket_name=self.bucket_name,
-                project=self.project,
-                local_dir=self.local_dir,
-            )
+    @classmethod
+    def from_blob_names(
+        cls,
+        blob_names: Sequence[str],
+        loader: callable = None,
+        transform: callable = None,
+        *args,
+        **kwargs,
+    ):
+        if not isinstance(blob_names, PandasSeriesColumn):
+            blob_names = PandasSeriesColumn(blob_names)
+        return cls(
+            blob_names=blob_names,
+            loader=loader,
+            transform=transform,
+            *args,
+            **kwargs,
+        )
 
     @classmethod
     def _state_keys(cls) -> set:
         """List of attributes that describe the state of the object."""
-        return super()._state_keys() | {"bucket_name", "project", "local_dir"}
+        return super()._state_keys() | {"bucket_name", "project", "local_dir", "writer"}
 
-    @staticmethod
-    def concat(columns: Sequence[GCSImageColumn]):
-        loader, transform = (
-            columns[0].loader,
-            columns[0].transform,
-        )
 
-        return GCSImageColumn(
-            filepaths=pd.concat([c.data for c in columns]),
-            loader=loader,
-            transform=transform,
-            bucket_name=columns[0].bucket_name,
-            project=columns[0].project,
-            local_dir=columns[0].local_dir,
-        )
+    def _set_state(self, state: dict = None):
+        if state is not None:
+            self.__dict__.update(state)
+        storage_client = storage.Client(project=self.project)
+        self.bucket = storage_client.bucket(self.bucket_name, user_project=self.project)
+
