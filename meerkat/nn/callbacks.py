3cf6f8e436441a2cb998d3670e20e452b358eae3
Priya <priyamis@cse.iitk.ac.in>
2021-08-09 19:24:51 +0530

    register forward hook at validation_epoch_start

--- meerkat/nn/callbacks.py
+++ meerkat/nn/callbacks.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from meerkat.datapanel import DataPanel
-from meerkat.nn.activation import activation
+from meerkat.nn.activation import ActivationOp
 from meerkat.tools.lazy_loader import LazyLoader
 
 pl = LazyLoader("pytorch_lightning")
@@ -21,15 +21,17 @@ class ActivationCallback(pl.callbacks.Callback):
         self.input_columns = input_columns
         self.batch_size = batch_size
 
+    def on_validation_epoch_start(self, trainer, pl_module):
+        self.activation_op = ActivationOp(
+            pl_module.model, self.target_module, pl_module.device
+        )
+
     def on_validation_epoch_end(self, trainer, pl_module):
 
-        activations = activation(
-            model=pl_module.model,
+        activations = self.activation_op.activation(
             dataset=self.val_dp,
-            target_module=self.target_module,
             input_columns=self.input_columns,
             forward=False,
-            device=pl_module.device,
             batch_size=self.batch_size,
         )
 
