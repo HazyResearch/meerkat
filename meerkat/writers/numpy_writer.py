1fa53ba083597b545fdd025378e8d2bee1b32d0d
Sabri Eyuboglu <32822771+seyuboglu@users.noreply.github.com>
2021-08-16 11:12:05 -0700

    Set writer.path in open (#130)

--- meerkat/writers/numpy_writer.py
+++ meerkat/writers/numpy_writer.py
@@ -53,6 +53,7 @@ class NumpyMemmapWriter(AbstractWriter):
         # Open the file as a memmap
         self.file = np.memmap(path, dtype=dtype, mode=mode, shape=shape)
         self._pointer = 0
+        self.path = path
 
     def write(self, arr, **kwargs) -> None:
         self.file[self._pointer : self._pointer + len(arr)] = arr
