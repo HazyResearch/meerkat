8c961c243705a216a85470ffad94131bddf64d36
khaledsaab <ksaab@stanford.edu>
2021-08-29 14:57:03 -0700

    integrating eeg data

--- meerkat/contrib/eeg/__init__.py
+++ meerkat/contrib/eeg/__init__.py
@@ -3,7 +3,7 @@ import os
 
 import meerkat as mk
 
-from meerkat.contrib.eeg.data_utils import computeSliceMatrix
+from .data_utils import computeSliceMatrix
 
 logger = logging.getLogger(__name__)
 from tqdm import tqdm
--- meerkat/contrib/eeg/data_utils.py
+++ meerkat/contrib/eeg/data_utils.py
@@ -1,5 +1,5 @@
-import numpy as np
 import h5py
+import numpy as np
 
 FREQUENCY = 200
 
