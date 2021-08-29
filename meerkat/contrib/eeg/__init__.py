2363612804d13bbda8ae98f8b89c321222f91c9b
khaledsaab <ksaab@stanford.edu>
2021-08-29 14:36:50 -0700

    integrated eeg dataset for TUH

---
+++ meerkat/contrib/eeg/__init__.py
@@ -0,0 +1,83 @@
+import logging
+import os
+
+import pandas as pd
+import meerkat as mk
+
+from meerkat.contrib.eeg.data_utils import computeSliceMatrix
+
+logger = logging.getLogger(__name__)
+from tqdm import tqdm
+
+
+def build_eeg_dp(
+    dataset_dir: str,
+    raw_dataset_dir: str,
+    clip_len: int = 60,
+    step_size: int = 1,
+    stride: int = 60,
+):
+    """
+    Builds a `DataPanel` for accessing EEG data.
+
+    Currently only supports TUH dataset for seq-seq prediction.
+    Future TODO: integrating stanford dataset with weak seq-seq labels
+
+    Args:
+        dataset_dir (str): A local directory where the preprocessed (h5) EEG data are stored.
+        raw_dataset_dir (str): A local directory where the original (edf) EEG data are stored.
+        clip_len (int): Number of seconds in an EEG clip
+        step_size (int): Number of seconds in a single 'step'
+        stride (int):  Number of seconds in the stride when extracting clips from signals
+    """
+
+    # retrieve paths of all edf files in the raw_dataset_dir
+    edf_files = []
+    for path, subdirs, files in os.walk(raw_dataset_dir):
+        for name in files:
+            if ".edf" in name:
+                edf_files.append(os.path.join(path, name))
+
+    data = []
+    dirname = os.path.dirname(__file__)
+    for split in ["train", "dev", "test"]:
+        fm_name = split + "_cliplen" + str(clip_len) + "_stride" + str(stride)
+        if split == "train":
+            fm_name += "_balanced"
+        file_marker = os.path.join(
+            os.path.join(dirname, "file_markers"),
+            fm_name + ".txt",
+        )
+
+        with open(file_marker, "r") as f:
+            file_str = f.readlines()
+        file_tuples = [curr_str.strip("\n").split(",") for curr_str in file_str]
+
+        for (edf_fn, clip_idx, _) in tqdm(file_tuples, total=len(file_tuples)):
+            filepath = [file for file in edf_files if edf_fn in file]
+            filepath = filepath[0]
+            file_id = edf_fn.split(".edf")[0]
+
+            eeg, sequence_sz, binary_sz = computeSliceMatrix(
+                h5_fn=os.path.join(dataset_dir, edf_fn.split(".edf")[0] + ".h5"),
+                edf_fn=filepath,
+                clip_idx=int(clip_idx),
+                time_step_size=step_size,
+                clip_len=clip_len,
+                stride=stride,
+            )
+
+            row_df = {
+                "filepath": filepath,
+                "file_id": file_id,
+                "eeg": eeg,
+                "sequence_sz": sequence_sz,
+                "binary_sz": binary_sz,
+                "split": split,
+            }
+
+            data.append(row_df)
+
+    dp = mk.DataPanel(data)
+
+    return dp
---
+++ meerkat/contrib/eeg/data_utils.py
@@ -0,0 +1,134 @@
+import numpy as np
+import h5py
+
+INCLUDED_CHANNELS = [
+    "EEG FP1",
+    "EEG FP2",
+    "EEG F3",
+    "EEG F4",
+    "EEG C3",
+    "EEG C4",
+    "EEG P3",
+    "EEG P4",
+    "EEG O1",
+    "EEG O2",
+    "EEG F7",
+    "EEG F8",
+    "EEG T3",
+    "EEG T4",
+    "EEG T5",
+    "EEG T6",
+    "EEG FZ",
+    "EEG CZ",
+    "EEG PZ",
+]
+
+FREQUENCY = 200
+
+
+def computeSliceMatrix(
+    edf_fn,
+    clip_idx,
+    channel_names=INCLUDED_CHANNELS,
+    h5_fn=None,
+    time_step_size=1,
+    clip_len=60,
+    stride=60,
+):
+    """
+    Comvert entire EEG sequence into clips of length clip_len
+    Args:
+        edf_fn: edf/eeghdf file name, full path
+        channel_names: list of channel names
+        clip_idx: index of current clip/sliding window, int
+        h5_fn: file name of resampled signal h5 file (full path)
+        time_step_size: length of each time step, in seconds, int
+        clip_len: sliding window size or EEG clip length, in seconds, int
+        stride: stride size, by how many seconds the sliding window moves, int
+    Returns:
+        eeg_clip: EEG clip, shape (clip_len//time_step_size, num_channels, time_step_size*freq)
+        seizure_labels: per-time-step seizure labels, shape (clip_len//time_step_size,)
+        is_seizure: overall label, 1 if at least one seizure within this clip, otherwise 0
+    """
+
+    physical_clip_len = int(FREQUENCY * clip_len)
+
+    start_window = clip_idx * FREQUENCY * stride
+
+    with h5py.File(h5_fn, "r") as f:
+        signal_array = f["resampled_signal"][()]
+        resampled_freq = f["resample_freq"][()]
+        assert resampled_freq == FREQUENCY
+
+    # (num_channels, physical_clip_len)
+
+    end_window = np.minimum(signal_array.shape[-1], start_window + physical_clip_len)
+    curr_slc = signal_array[:, start_window:end_window]  # (num_channels, FREQ*clip_len)
+    physical_time_step_size = int(FREQUENCY * time_step_size)
+
+    start_time_step = 0
+    time_steps = []
+    while start_time_step <= curr_slc.shape[1] - physical_time_step_size:
+        end_time_step = start_time_step + physical_time_step_size
+        # (num_channels, physical_time_step_size)
+        curr_time_step = curr_slc[:, start_time_step:end_time_step]
+
+        time_steps.append(curr_time_step)
+        start_time_step = end_time_step
+
+    eeg_clip = np.stack(time_steps, axis=0)
+
+    # get seizure times, take min_sz_len into account
+    if ".edf" in edf_fn:
+        seizure_times = getSeizureTimes(edf_fn.split(".edf")[0])
+    else:
+        raise NotImplementedError
+
+    # get per-time-step seizure labels
+    num_time_steps = eeg_clip.shape[0]
+    seizure_labels = np.zeros((num_time_steps)).astype(int)
+    is_seizure = 0
+    for t in seizure_times:
+        start_t = int(t[0] * FREQUENCY)
+        end_t = int(t[1] * FREQUENCY)
+        if not ((end_window < start_t) or (start_window > end_t)):
+            is_seizure = 1
+
+            start_t_sec = int(t[0])  # start of seizure in int seconds
+            end_t_sec = int(t[1])  # end of seizure in int seconds
+
+            # shift start_t_sec and end_t_sec so that they start at current clip
+            start_t_sec = np.maximum(0, start_t_sec - int(start_window / FREQUENCY))
+            end_t_sec = np.minimum(clip_len, end_t_sec - int(start_window / FREQUENCY))
+            # print("start_t_sec: {}; end_t_sec: {}".format(start_t_sec, end_t_sec))
+
+            # time step size may not be 1-sec
+            start_time_step = int(np.floor(start_t_sec / time_step_size))
+            end_time_step = int(np.ceil(end_t_sec / time_step_size))
+
+            seizure_labels[start_time_step:end_time_step] = 1
+
+    return eeg_clip, seizure_labels, is_seizure
+
+
+def getSeizureTimes(file_name):
+    """
+    Args:
+        file_name: file name of .edf file etc.
+    Returns:
+        seizure_times: list of times of seizure onset in seconds
+    """
+    tse_file = file_name.split(".edf")[0] + ".tse_bi"
+
+    seizure_times = []
+    with open(tse_file) as f:
+        for line in f.readlines():
+            if "seiz" in line:  # if seizure
+                # seizure start and end time
+                seizure_times.append(
+                    [
+                        float(line.strip().split(" ")[0]),
+                        float(line.strip().split(" ")[1]),
+                    ]
+                )
+    return seizure_times
