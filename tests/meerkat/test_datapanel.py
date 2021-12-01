ac9f59ebd4c2092bb0cedf79776343b748f7e293
Sabri Eyuboglu <eyuboglu@stanford.edu>
2021-12-01 15:50:50 -0500

    Fix from_huggingface and add tests

--- tests/meerkat/test_datapanel.py
+++ tests/meerkat/test_datapanel.py
@@ -805,6 +805,26 @@ class TestDataPanel:
                 data_to_compare = dp_new[k]._data
             assert data_to_compare == data[k]
 
+    def test_from_huggingface(self, tmpdir: str):
+        # Returns a dataset dict
+        dp = DataPanel.from_huggingface(
+            "hf-internal-testing/fixtures_ade20k",
+            cache_dir=tmpdir,
+            revision="ddd1a5c",
+        )["test"]
+        assert len(dp) == 4
+        assert len(dp.columns) == 3
+
+        # Returns a dataset
+        dp = DataPanel.from_huggingface(
+            "hf-internal-testing/fixtures_ade20k",
+            cache_dir=tmpdir,
+            revision="ddd1a5c",
+            split="test",
+        )
+        assert len(dp) == 4
+        assert len(dp.columns) == 3
+
     def test_from_jsonl(self):
         # Build jsonl file
         temp_f = tempfile.NamedTemporaryFile()
