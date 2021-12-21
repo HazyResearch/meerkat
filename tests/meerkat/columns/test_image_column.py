cd0dbc627b8bff832d1b9cc0ca99a2c8e140f5c6
Sabri Eyuboglu <eyuboglu@stanford.edu>
2021-12-21 11:47:27 -0500

    Add downloader to ImageColumn

--- tests/meerkat/columns/test_image_column.py
+++ tests/meerkat/columns/test_image_column.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 import os
 from typing import List, Union
 
+import dill
 import numpy as np
 import pandas as pd
 import pytest
@@ -15,7 +16,7 @@ from torchvision.transforms.functional import to_tensor
 import meerkat
 from meerkat import ImageColumn
 from meerkat.columns.abstract import AbstractColumn
-from meerkat.columns.image_column import ImageCell
+from meerkat.columns.image_column import Downloader, ImageCell
 from meerkat.columns.lambda_column import LambdaCell
 from meerkat.columns.list_column import ListColumn
 from meerkat.columns.pandas_column import PandasSeriesColumn
@@ -26,7 +27,10 @@ from .abstract import AbstractColumnTestBed, TestAbstractColumn
 
 class ImageColumnTestBed(AbstractColumnTestBed):
 
-    DEFAULT_CONFIG = {"transform": [True, False], "use_base_dir": [True, False]}
+    DEFAULT_CONFIG = {
+        "transform": [True, False],
+        "use_base_dir": [True, False],
+    }
 
     def __init__(
         self,
@@ -309,3 +313,60 @@ class TestImageColumn(TestAbstractColumn):
             assert callable(formatter)
         else:
             assert formatter is None
+
+
+def test_downloader(monkeypatch, tmpdir):
+    import urllib
+
+    ims = []
+
+    def patched_urlretrieve(url, filename):
+        img_array = np.ones((4, 4, 3)).astype(np.uint8)
+        im = Image.fromarray(img_array)
+        ims.append(im)
+        im.save(filename)
+
+    monkeypatch.setattr(urllib.request, "urlretrieve", patched_urlretrieve)
+
+    downloader = Downloader(cache_dir=os.path.join(tmpdir, "cache"))
+
+    out = downloader("https://test.com/dir/2.jpg")
+
+    assert os.path.exists(os.path.join(tmpdir, "cache", "test.com/dir/2.jpg"))
+    assert (np.array(out) == np.array(ims[0])).all()
+
+    out = downloader("https://test.com/dir/2.jpg")
+    assert len(ims) == 1
+
+    out = downloader("https://test.com/dir/3.jpg")
+    assert len(ims) == 2
+
+    dill.dump(downloader, open(os.path.join(tmpdir, "cache", "downloader.pkl"), "wb"))
+    downloader = dill.load(open(os.path.join(tmpdir, "cache", "downloader.pkl"), "rb"))
+
+    # reload
+    downloader = Downloader(cache_dir=os.path.join(tmpdir, "cache"))
+
+
+def test_unsuccessful_download(monkeypatch, tmpdir):
+    import urllib
+
+    def patched_urlretrieve(url, filename):
+        raise urllib.error.HTTPError(url, 404, "Not found", None, None)
+
+    monkeypatch.setattr(urllib.request, "urlretrieve", patched_urlretrieve)
+
+    downloader = Downloader(cache_dir=os.path.join(tmpdir, "cache"))
+
+    out = downloader("https://test.com/dir/2.jpg")
+    assert out is None
+
+
+def test_serialize_downloader(tmpdir):
+    downloader = Downloader(cache_dir="cache")
+
+    dill.dump(downloader, open(os.path.join(tmpdir, "downloader.pkl"), "wb"))
+
+    downloader = dill.load(open(os.path.join(tmpdir, "downloader.pkl"), "rb"))
+
+    assert downloader.cache_dir == "cache"
