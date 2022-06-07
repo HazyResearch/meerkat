67484cd53e5dafae646e1cbcf4cc7adc870dd9b4
Sam Randall <1billionmore@gmail.com>
2022-06-07 12:53:10 -0700

    add test

--- tests/meerkat/ops/test_groupby.py
+++ tests/meerkat/ops/test_groupby.py
@@ -64,6 +64,20 @@ def test_group_by_integer_type():
     assertNumpyArrayEquality(out["b"].data, np.array([2.5, (2 + 3 + 6) / 3, 6]))
     assertNumpyArrayEquality(out["a"].data , np.array([1, 2, 3]))
 
+def test_group_by_integer_type_as_prop():
+    dp = DataPanel({
+        'a': NumpyArrayColumn([1, 2, 2, 1, 3, 2, 3]),
+        'name': NumpyArrayColumn(np.array(['a', 'b', 'a', 'c', 'b', 'd', 'd'], dtype = str)),
+        'b': NumpyArrayColumn([1, 2, 3, 4, 5, 6, 7]),
+        'c': NumpyArrayColumn([1.0, 3.2, 2.1, 4.3, 5.4, 6.5, 7.6])
+    })
+
+    df = dp.groupby('a')
+    out = df["b"].mean()
+
+    assertNumpyArrayEquality(out["b"].data, np.array([2.5, (2 + 3 + 6) / 3, 6]))
+    assertNumpyArrayEquality(out["a"].data , np.array([1, 2, 3]))
+
 def test_group_by_tensor_key():
 
     dp = DataPanel({
