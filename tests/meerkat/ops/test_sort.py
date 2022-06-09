cd80f0198a480a402f326d52720bc6ac87472d9d
Hannah Kim <hkim24@stanford.edu>
2022-06-09 11:49:57 -0700

    Final test and sort implementation changes

--- tests/meerkat/ops/test_sort.py
+++ tests/meerkat/ops/test_sort.py
@@ -24,6 +24,17 @@ def make_test_dp(by: Union[str, List[str]],
    test = dp.sort(by=by, ascending=ascending)
    return test
 
+def make_tiebreaker_test_dp(by: Union[str, List[str]],
+                  ascending: Union[bool, List[bool]] = True,):
+   dp = mk.DataPanel({
+      'tensor': mk.TensorColumn([3,2,1]),
+      'pandas':  mk.PandasSeriesColumn([9,7,9]),
+      'numpy': mk.NumpyArrayColumn([4,4,6]), 
+      }) 
+   test = dp.sort(by=by, ascending=ascending)
+   return test
+
+
 ######## SINGLE COLUMN TESTS ########
 
 # Testing all columns after sorting by an ascending tensor column
@@ -65,58 +76,34 @@ def test_sort_single_numpy_column_descending():
 
 # Testing all columns after sorting with multiple ascending columns (numpy and tensor)
 def test_sort_numpy_and_tensor_ascending():
-    test = make_test_dp(by=["numpy", "tensor"], ascending=True)
-    assert (test["tensor"] == mk.TensorColumn([1,3,2])).all() and (test["pandas"] == mk.PandasSeriesColumn([8,9, 7])).all() and (test["numpy"] == mk.NumpyArrayColumn([4,5, 6])).all()
-    
+    test = make_tiebreaker_test_dp(by=["numpy", "tensor"], ascending=True)
+    assert (test["tensor"] == mk.TensorColumn([2,3,1])).all() and (test["pandas"] == mk.PandasSeriesColumn([7,9,9])).all() and (test["numpy"] == mk.NumpyArrayColumn([4,4,6])).all()
+        
 
 # Testing all columns after sorting with multiple ascending columns (numpy and pandas)
 def test_sort_numpy_and_pandas_ascending():
-    test = make_test_dp(by=["numpy", "pandas"], ascending=True)
-    assert (test["tensor"] == mk.TensorColumn([1,3,2])).all() and (test["pandas"] == mk.PandasSeriesColumn([8,9, 7])).all() and (test["numpy"] == mk.NumpyArrayColumn([4,5, 6])).all()
+    test = make_tiebreaker_test_dp(by=["numpy", "pandas"], ascending=True)
+    assert (test["tensor"] == mk.TensorColumn([2,3,1])).all() and (test["pandas"] == mk.PandasSeriesColumn([7,9, 9])).all() and (test["numpy"] == mk.NumpyArrayColumn([4,4, 6])).all()
 
 
 # Testing all columns after sorting with multiple ascending columns (numpy and pandas and tensor)
 def test_sort_numpy_and_pandas_and_tensor_ascending():
-    test = make_test_dp(by=["numpy", "pandas", "tensor"], ascending=True)
-    assert (test["tensor"] == mk.TensorColumn([1,3,2])).all() and (test["pandas"] == mk.PandasSeriesColumn([8,9, 7])).all() and (test["numpy"] == mk.NumpyArrayColumn([4,5, 6])).all()
-        
-
-# Testing all columns after sorting with multiple ascending columns (tensor and numpy)
-def test_sort_tensor_and_numpy_ascending():
-    test = make_test_dp(by=["tensor", "numpy"], ascending=True)
-    assert (test["tensor"] == mk.TensorColumn([1,2,3])).all() and (test["pandas"] == mk.PandasSeriesColumn([8,7,9])).all() and (test["numpy"] == mk.NumpyArrayColumn([4,6,5])).all()
+   dp = mk.DataPanel({
+      'tensor': mk.TensorColumn([3,2,1]),
+      'pandas':  mk.PandasSeriesColumn([9,7,7]),
+      'numpy': mk.NumpyArrayColumn([6,4,4]), 
+      })  
+   test = dp.sort(by=["numpy", "pandas", "tensor"], ascending=True)
+   assert (test["tensor"] == mk.TensorColumn([1,2,3])).all() and (test["pandas"] == mk.PandasSeriesColumn([7,7, 9])).all() and (test["numpy"] == mk.NumpyArrayColumn([4,4, 6])).all()
         
 
 # Testing all columns after sorting with multiple ascending columns (tensor and pandas)
-def test_sort_tensor_and_pandas_ascending():
-    test = make_test_dp(by=["tensor", "pandas"], ascending=True)
-    assert (test["tensor"] == mk.TensorColumn([1,2,3])).all() and (test["pandas"] == mk.PandasSeriesColumn([8,7,9])).all() and (test["numpy"] == mk.NumpyArrayColumn([4,6,5])).all()
+def test_sort_tensor_and_pandas_descending():
+   dp = mk.DataPanel({
+      'tensor': mk.TensorColumn([3,2,2]),
+      'pandas':  mk.PandasSeriesColumn([9,8,7]),
+      'numpy': mk.NumpyArrayColumn([6,4,4]), 
+      })  
+   test = dp.sort(by=["tensor", "pandas"], ascending=False)
+   assert (test["tensor"] == mk.TensorColumn([3,2,2])).all() and (test["pandas"] == mk.PandasSeriesColumn([9,8,7])).all() and (test["numpy"] == mk.NumpyArrayColumn([6,4,4])).all()
         
-# Testing all columns after sorting with multiple ascending columns (tensor and pandas, numpy)
-def test_sort_tensor_and_pandas_and_numpy_ascending():
-    test = make_test_dp(by=["tensor", "pandas", "numpy"], ascending=True)
-    assert (test["tensor"] == mk.TensorColumn([1,2,3])).all() and (test["pandas"] == mk.PandasSeriesColumn([8,7,9])).all() and (test["numpy"] == mk.NumpyArrayColumn([4,6,5])).all()
-        
-
-# Testing all columns after sorting with multiple ascending columns (pandas and numpy)
-def test_sort_pandas_and_numpy_ascending():
-    test = make_test_dp(by=["pandas", "numpy"], ascending=True)
-    assert (test["tensor"] == mk.TensorColumn([2,1,3])).all() and (test["pandas"] == mk.PandasSeriesColumn([7,8,9])).all() and (test["numpy"] == mk.NumpyArrayColumn([6,4,5])).all()
-
-# Testing all columns after sorting with multiple ascending columns (pandas and tensor)
-def test_sort_pandas_and_tensor_ascending():
-    test = make_test_dp(by=["pandas", "tensor"], ascending=True)
-    assert (test["tensor"] == mk.TensorColumn([2,1,3])).all() and (test["pandas"] == mk.PandasSeriesColumn([7,8,9])).all() and (test["numpy"] == mk.NumpyArrayColumn([6,4,5])).all()
-        
-# Testing all columns after sorting with multiple ascending columns (pandas and tensor and numpy)
-def test_sort_pandas_and_tensor_and_numpy_ascending():
-    test = make_test_dp(by=["pandas", "tensor", "numpy"], ascending=True)
-    assert (test["tensor"] == mk.TensorColumn([2,1,3])).all() and (test["pandas"] == mk.PandasSeriesColumn([7,8,9])).all() and (test["numpy"] == mk.NumpyArrayColumn([6,4,5])).all()
-        
-# Testing all columns after sorting with multiple ascending columns (pandas and tensor)
-def test_sort_pandas_and_tensor_descending():
-    test = make_test_dp(by=["pandas", "tensor"], ascending=False)
-    assert (test["tensor"] == mk.TensorColumn([3,1,2])).all() and (test["pandas"] == mk.PandasSeriesColumn([9,8,7])).all() and (test["numpy"] == mk.NumpyArrayColumn([5,4,6])).all()
-        
-    
-    
