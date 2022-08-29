c49f58f528dc46708843fe99e7f257fb38bba26e
Karan Goel <kgoel93@gmail.com>
2022-08-28 19:56:03 -0700

    Add responsive grid layout

--- meerkat/interactive/app/src/routes/interface.svelte
+++ meerkat/interactive/app/src/routes/interface.svelte
@@ -16,13 +16,18 @@
 <script lang="ts">
 	import { get, writable } from 'svelte/store';
 	import { api_url } from './network/stores';
-	import { getContext, setContext } from 'svelte';
+	import { setContext } from 'svelte';
 
 	import { global_stores } from '$lib/components/blanks/stores';
 	import { get_schema, MatchCriterion } from '$lib/api/datapanel';
 
 	import { onMount } from 'svelte';
-	import { post, modify } from '$lib/utils/requests';
+	import { modify } from '$lib/utils/requests';
+
+	import Grid from "svelte-grid";
+	import gridHelp from "svelte-grid/build/helper/index";
+	import Draggable from "carbon-icons-svelte/lib/Draggable.svelte";
+
 
 	export let config: any;
 
@@ -30,16 +35,12 @@
 		let schema = await get_schema($api_url, box_id);
 		return schema;
 	};
-	const _schema = writable(schema);
-	$: $_schema = schema;
-
+	
 	$: add = async (box_id: string, column_name: string) => {
 		let modifications = await modify(`${$api_url}/dp/${box_id}/add`, { column: column_name });
 		return modifications;
 	};
-	const _add = writable(add);
-	$: $_add = add;
-
+	
 	$: match = async (box_id: string, criterion: MatchCriterion) => {
 		let modifications = await modify(`${$api_url}/dp/${box_id}/match`, {
 			input: criterion.column,
@@ -47,7 +48,13 @@
 		});
 		return modifications;
 	};
+
+	const _schema = writable(schema);
+	const _add = writable(add);
 	const _match = writable(match);
+
+	$: $_schema = schema;
+	$: $_add = add;
 	$: $_match = match;
 
 	$: context = {
@@ -62,6 +69,7 @@
 		component_refs[component_id] = undefined;
 	}
 
+	let document_container;
 	let imported_components: any = {};
 	onMount(async () => {
 		// for loop
@@ -72,10 +80,15 @@
 				await import(`$lib/component/${component_name.toLowerCase()}/${component_name}.svelte`)
 			).default;
 		}
+
+		document_container = document.documentElement;
 	});
+
+	let grid_items = [];
 
 	for (let i = 0; i < config.components.length; i++) {
 		let component = config.components[i];
+		// Define the stores
 		for (let [k, v] of Object.entries(component.props)) {
 			if (v) {
 				if (v.store_id !== undefined) {
@@ -94,12 +107,61 @@
 				}
 			}
 		}
+
+		// Setup for responsive grid layout
+		grid_items.push({
+			6: gridHelp.item({
+				x: 0,
+				y: 2 * i,
+				w: 6,
+				h: 2,
+				customDragger: true
+			}),
+			id: i,
+		})
+
 	}
+
+	const cols = [
+		[ 1200, 6 ],
+	];
+
 </script>
 
-<div class="flex-col space-y-3">
+<style>
+	.dragger {
+		@apply opacity-0 hover:opacity-100 absolute top-0 left-0 select-none cursor-grab bg-violet-200 text-violet-600;
+	}
+</style>
+
+<div class="w-full">
+	<Grid 
+		bind:items={grid_items} 
+		rowHeight={50} 
+		let:index 
+		let:item 
+		let:dataItem 
+		let:movePointerDown 
+		{cols}
+		fastStart={true}
+		fillSpace={true}
+		scroller={document_container}
+	>
+		{@const {component, component_id, props} = config.components[index]}
+		{@const Component = imported_components[component]}
+		<svelte:component this={Component} {...props} />
+		{#if item.customDragger}
+			<div class="dragger" on:pointerdown={movePointerDown}>
+				<Draggable size={20}/>
+			</div>
+		{/if}
+	</Grid>
+</div>
+  
+
+<!-- <div class="flex-col space-y-3">
 	{#each config.components as { component, component_id, props }}
 		{@const Component = imported_components[component]}
 		<svelte:component this={Component} {...props} />
 	{/each}
-</div>
+</div> -->
