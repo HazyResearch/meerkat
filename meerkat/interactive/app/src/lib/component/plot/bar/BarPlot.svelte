a4b97ccd7149a83f036497bd174f5e7280c48be8
Karan Goel <kgoel93@gmail.com>
2022-09-21 14:59:58 -0700

    Reformat

--- meerkat/interactive/app/src/lib/component/plot/bar/BarPlot.svelte
+++ meerkat/interactive/app/src/lib/component/plot/bar/BarPlot.svelte
@@ -1,27 +1,23 @@
 <script lang="ts">
-	import { Html, LayerCake, Svg } from 'layercake';
 	import type { Point2D } from '$lib/components/plot/types';
-    import { scaleBand } from 'd3-scale';
+	import { scaleBand } from 'd3-scale';
+	import { Html, LayerCake, Svg } from 'layercake';
 
-
+	import Label from '$lib/components/plot/layercake/labels/Label.html.svelte';
 	import SvgAxisX from './axes/AxisX.svg.svelte';
 	import SvgAxisY from './axes/AxisY.svg.svelte';
-	import Label from '$lib/components/plot/layercake/labels/Label.html.svelte';
 	import SvgBar from './Bar.svg.svelte';
 
 	export let data: Array<Point2D>;
 
 	export let radius = 3;
-    export let padding: number = 10;
+	export let padding: number = 10;
 	export let selection: Array<number> = [];
 
-    export let xlabel: string = 'x label';
-    export let ylabel: string = 'y label';
-    
+	export let xlabel: string = 'x label';
+	export let ylabel: string = 'y label';
+
 	let height = 40 * data.length;
-
-
-
 </script>
 
 <div class="w-full" style:height={`${height}px`}>
@@ -31,20 +27,18 @@
 		padding={{ top: padding, right: padding, bottom: padding, left: padding }}
 		x="x"
 		y="y"
-        yScale={scaleBand().paddingInner([0.05])}
+		yScale={scaleBand().paddingInner([0.05])}
 		{data}
-        xDomain={[-0.4, 0.4]}
+		xDomain={[-0.4, 0.4]}
 	>
-        <Html>
-            <Label axis="x" label={xlabel}/>
-            <Label axis="y" label={ylabel}/>
-        </Html>
+		<Html>
+			<Label axis="x" label={xlabel} />
+			<Label axis="y" label={ylabel} />
+		</Html>
 		<Svg>
 			<SvgAxisX />
-			<SvgAxisY gridlines={false}/>
-			<SvgBar {radius} on:selection-change/>
+			<SvgAxisY gridlines={false} />
+			<SvgBar {radius} on:selection-change />
 		</Svg>
-        
 	</LayerCake>
 </div>
-
