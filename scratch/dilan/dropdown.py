761bb0f4201a08834ca843e9221c2d86c7c7d8d3
dilan-js <dilannana.js@gmail.com>
2022-10-28 17:50:25 -0700

    fixed the dropdown results for match

---
+++ scratch/dilan/dropdown.py
@@ -0,0 +1,29 @@
+import meerkat as mk
+
+dp = mk.get("imagenette",).lz[:100]
+dp_pivot = mk.gui.Pivot(dp)
+
+dp = mk.embed(
+    dp,
+    input="img",
+    batch_size=128,
+)
+
+match: mk.gui.Component = mk.gui.Match(
+    dp_pivot, 
+    against="img",
+    col="label"
+)
+
+sorted_box = mk.sort(dp_pivot, by=match.col, ascending=False)
+
+gallery = mk.gui.Gallery(
+    sorted_box,
+    main_column="img",
+    tag_columns=["label"],
+)
+
+mk.gui.start()
+mk.gui.Interface(
+    components=[match, gallery]
+).launch()
