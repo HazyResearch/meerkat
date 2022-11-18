9965b641fef122368f81e9e3c71e536c45ab507d
Karan Goel <kgoel93@gmail.com>
2022-11-17 20:06:28 -0800

    Update tooltip for FancyTick

--- meerkat/interactive/app/src/lib/component/plot/bar/axes/FancyTick.svelte
+++ meerkat/interactive/app/src/lib/component/plot/bar/axes/FancyTick.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import Close from 'carbon-icons-svelte/lib/Close.svelte';
-	import { createEventDispatcher } from 'svelte';
-	import { getContext } from 'svelte';
+	import { createEventDispatcher, getContext } from 'svelte';
+	import { createTippy } from 'svelte-tippy';
+	import { followCursor } from 'tippy.js';
 
 	const dispatch = createEventDispatcher();
 
@@ -12,37 +13,54 @@
 
 	$: can_remove = getContext('can_remove');
 
-	let hover = false;
+	let tippy = (node: HTMLElement, parameters: any = null) => {};
+
+	tippy = createTippy({
+		placement: 'auto',
+		allowHTML: true,
+		theme: 'fancy-tick-tooltip',
+		followCursor: 'vertical',
+		plugins: [followCursor],
+		duration: [0, 0],
+		maxWidth: '95vw',
+		interactive: true
+	});
 </script>
 
-<div class="relative" on:mouseenter={() => (hover = true)} on:mouseleave={() => (hover = false)}>
-	{#if hover}
-		<div class="absolute top-0 left-0 bg-slate-100 z-40 rounded px-2 py-1 shadow-md h-fit">
-			<div class="grid grid-cols-[1fr_auto]">
-				<div>
-					{name}
-				</div>
-				{#if can_remove}
-					<div
-						class="font-bold text-red-400 hover:text-red-600"
-						on:click={() => dispatch('remove', id)}
-					>
-						<Close />
-					</div>
-				{/if}
-			</div>
-			<div class="grid grid-flow-row grid-cols-2 space-x-1">
-				<div class="font-bold">Name</div>
-				<div>{name}</div>
-				<div class="font-bold">Count</div>
-				<div>{size}</div>
-				<div>ID</div>
-				<div>{id}</div>
-			</div>
-		</div>
-	{/if}
-
-	<div class="bg-slate-100 z-10 rounded px-2 py-1 w-fit">
+<div class="relative">
+	<div
+		class="bg-slate-100 z-10 rounded px-2 py-1 text-center"
+		use:tippy={{ content: document.getElementById(`fancy-tick-${id}`)?.innerHTML }}
+	>
 		{name}
 	</div>
+
+	<div id="fancy-tick-{id}" class="hidden">
+		<div class="grid grid-cols-[1fr_auto] items-center">
+			<!-- <div>{name}</div> -->
+			{name}
+			{#if can_remove}
+				<div
+					class="font-bold text-red-600 hover:text-red-800"
+					on:click={() => dispatch('remove', id)}
+				>
+					<Close size=32/>
+				</div>
+			{/if}
+		</div>
+		<div class="mt-1 grid grid-flow-row grid-cols-2 space-x-1">
+			<div class="font-bold">Count</div>
+			<div>{size}</div>
+			<div class="font-bold">ID</div>
+			<div>{id}</div>
+		</div>
+	</div>
 </div>
+
+<style>
+	/* CSS for the tooltip */
+	:global(.tippy-box[data-theme='fancy-tick-tooltip']) {
+		@apply py-2 px-4 text-base font-mono rounded shadow-md h-fit;
+		@apply text-white bg-violet-500;
+	}
+</style>
