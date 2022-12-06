003b1f9ba461644220220350b736c4f29dc18dd2
Sabri Eyuboglu <eyuboglu@stanford.edu>
2022-12-06 18:06:42 -0500

    Update composed script

--- scratch/sabri/11-28_composed_df.py
+++ scratch/sabri/11-28_composed_df.py
@@ -1,8 +1,5 @@
 import meerkat as mk
 
-from meerkat.interactive.app.src.lib.component.mocha.test import Test
-# from mocha.component.test import Test
-
 df = mk.get("imagenette")
 
 tabs = mk.gui.Tabs(
@@ -16,16 +13,8 @@ tabs = mk.gui.Tabs(
     }
 )
 
-gallery = Test(
-    gallery=mk.gui.Gallery(
-        df=df,
-        main_column="img",
-        tag_columns=["label"],
-    )
-)
 
-
-mk.gui.start(shareable=False)
+mk.gui.start(shareable=False, dev=False)
 mk.gui.Interface(
-    component=gallery,
+    component=tabs,
 ).launch()
