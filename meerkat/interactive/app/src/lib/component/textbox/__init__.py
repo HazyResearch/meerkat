f7917f5a9cd5b337dde44b0c17738d6331858850
Karan Goel <kgoel93@gmail.com>
2022-12-08 14:44:17 -0800

    Add imports

--- meerkat/interactive/app/src/lib/component/textbox/__init__.py
+++ meerkat/interactive/app/src/lib/component/textbox/__init__.py
@@ -1,23 +1,9 @@
-from meerkat.interactive.graph import make_store
+from meerkat.interactive.graph import Store
 
 from ..abstract import Component
 
 
 class Textbox(Component):
 
-    name = "Textbox"
-
-    def __init__(
-        self,
-        title: str = "",
-    ):
-        super().__init__()
-        self.text = make_store("")
-        self.title = title
-
-    @property
-    def props(self):
-        return {
-            "text": self.text.config,
-            "title": self.title,
-        }
+    text: Store[str]
+    title: str = ""
