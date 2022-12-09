a5f7a5581eee3578bde746bf7185cb91e01a32b6
Karan Goel <kgoel93@gmail.com>
2022-12-08 22:09:31 -0800

    Start fleshing out cmd line api

--- meerkat/cli/commands.py
+++ meerkat/cli/commands.py
@@ -1,7 +1,30 @@
-import os
-import sys
-
 import typer
 
-print("Hello World")
-print()
+app = typer.Typer()
+
+
+@app.command()
+def init():
+    typer.echo("Initializing Meerkat")
+
+
+@app.command()
+def run(
+    script_path: str,
+    dev: bool = False,
+    shareable: bool = False,
+    port: int = 5000,
+):
+    typer.echo(f"Running {script_path}")
+    typer.echo(f"dev: {dev}")
+    typer.echo(f"shareable: {shareable}")
+    typer.echo(f"port: {port}")
+
+
+@app.command()
+def install():
+    typer.echo("Installing Meerkat")
+
+
+if __name__ == "__main__":
+    app()
