a21bb25ffa14b2910e6273bd125d68872e7aaacb
Arjun Desai <arjunaashna@gmail.com>
2022-12-24 15:28:51 -0800

    fix match script

--- scratch/arjun/12-23_match-demo.py
+++ scratch/arjun/12-23_match-demo.py
@@ -4,24 +4,18 @@ IMAGE_COLUMN = "img"
 EMBED_COLUMN = "img_clip"
 
 df = mk.get("imagenette", version="160px")
-df = df[100:150]
 
 # Embed the image.
 df = mk.embed(df, input=IMAGE_COLUMN, out_col=EMBED_COLUMN)
 
 with mk.gui.react():
     # Match
-    match = mk.gui.Match(
-        df=df,
-        against=EMBED_COLUMN,
-        title="Search Examples",
-    )
+    match = mk.gui.Match(df=df, against=EMBED_COLUMN)
     examples_df = match(df)[0]
 
     # Sort
     df_sorted = mk.sort(data=examples_df, by=match.criterion.name, ascending=False)
 
-print(df_sorted)
 # Gallery
 gallery = mk.gui.Gallery(
     df=df_sorted,
@@ -29,6 +23,4 @@ gallery = mk.gui.Gallery(
 )
 
 mk.gui.start(shareable=False)
-mk.gui.Interface(
-    component=mk.gui.RowLayout(components=[match, gallery])
-).launch()
+mk.gui.Interface(component=mk.gui.RowLayout(components=[match, gallery])).launch()
