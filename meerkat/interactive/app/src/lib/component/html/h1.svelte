a38cc382189e0091e9a1af9cb7c6ef2aed402b8b
Karan Goel <kgoel93@gmail.com>
2023-01-21 12:58:36 -0800

    Add brace, html components

---
+++ meerkat/interactive/app/src/lib/component/html/h1.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let classes: string;
+	export let style: string;
+</script>
+
+<h1 class={classes} {style}>
+    <slot />
+</h1>
---
+++ meerkat/interactive/app/src/lib/layouts/Brace.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	export let data: any;
+</script>
+
+{data}
