f963d7aff5ccea09f46f6ffeda8e6a944cd97354
Arjun Desai <arjunaashna@gmail.com>
2023-01-21 13:59:05 -0800

    add toggle

--- meerkat/interactive/app/src/lib/component/toggle/Toggle.svelte
+++ meerkat/interactive/app/src/lib/component/toggle/Toggle.svelte
@@ -2,6 +2,7 @@
 	import { createEventDispatcher } from 'svelte';
 
 	export let title: string = '';
+	export let value: boolean;
 
 	let dispatch = createEventDispatcher();
 
@@ -18,6 +19,7 @@
 		on:change={(e) => {
 			dispatchToggle(e.target.checked);
 		}}
+		checked={value}
 	/>
 	<div
 		class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"
