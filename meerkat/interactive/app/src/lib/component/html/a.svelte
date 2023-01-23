00b03edede0255dc133cd69f40ac517ed1a0fbf7
Karan Goel <kgoel93@gmail.com>
2023-01-22 16:55:03 -0800

    Remove extra html components

--- meerkat/interactive/app/src/lib/component/html/a.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	export let classes: string;
-	export let style: string;
-	export let href: string;
-</script>
-
-<a class={classes} {style} {href}>
-	<slot />
-</a>
--- meerkat/interactive/app/src/lib/component/html/h1.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	export let classes: string;
-	export let style: string;
-</script>
-
-<h1 class={classes} {style}>
-    <slot />
-</h1>
