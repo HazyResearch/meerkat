ba927483b8bb39fe0cdd8e8b347caf6edb40748b
Karan Goel <kgoel93@gmail.com>
2023-01-25 23:33:30 -0800

    Update gpt index demo

--- demo/gpt-index.py
+++ demo/gpt-index.py
@@ -3,29 +3,35 @@ import os
 from gpt_index import GPTSimpleVectorIndex, SimpleDirectoryReader
 
 import meerkat as mk
+from meerkat.interactive.app.src.lib.component.fileupload import FileUpload
 
-dir_name = mk.gui.Store(None)
+dir = mk.gui.Store(None)
+query = mk.gui.Store("")
+savefilename = "gpt_index"
 
 
 @mk.gui.react()
-def load_index(dir_name: str) -> GPTSimpleVectorIndex:
-    if not dir_name:
+def load_index(dir: str, savefilename: str) -> GPTSimpleVectorIndex:
+    print(
+        f"Executing load_index with dir={type(dir)} and savefilename={type(savefilename)}"
+    )
+    if not dir:
         return None
 
-    if os.path.exists(os.path.join(dir_name, f"{dir_name}.json")):
+    if os.path.exists(os.path.join(dir, f"{savefilename}.json")):
         # load the index after savaing (maybe)
-        return GPTSimpleVectorIndex.load_from_disk(f"{dir_name}.json")
-    else:
-        raise FileNotFoundError("GPT index.json file not found.")
+        return GPTSimpleVectorIndex.load_from_disk(f"{savefilename}.json")
 
     # reader for docs
-    documents = SimpleDirectoryReader(dir_name).load_data()
+    documents = SimpleDirectoryReader(dir).load_data()
 
     # build the index
     index = GPTSimpleVectorIndex(documents)
 
     # save the index somewhere (maybe)
-    index.save_to_disk("file_path.json")
+    index.save_to_disk(os.path.join(dir, f"{savefilename}.json"))
+
+    print("Index created.")
 
     return index
 
@@ -37,21 +43,25 @@ def query_gpt_index(index: GPTSimpleVectorIndex, query: str):
     return index.query(query)
 
 
-index = load_index(dir_name=dir_name)
+index = load_index(dir=dir, savefilename=savefilename)
 
-picker = mk.gui.SomeComponent(
-    value=dir_name,
-)
+# Make a FileUpload component to select a directory
+fileupload_component = FileUpload(value=dir)
+query_component = mk.gui.Textbox(text=query)
 
-querier = mk.gui.GPTIndexQueryComponent(
-    # This "special" component sends an event called "query"
-    # that contains a payload with a key called `query` that contains
-    # the string query
-    on_query=query_gpt_index.partial(index=index),
+button = mk.gui.Button(
+    title="Query GPT-Index",
+    on_click=query_gpt_index.partial(index=index, query=query),
 )
 
 interface = mk.gui.Interface(
-    component=mk.gui.RowLayout(slots=[picker, querier]),
+    component=mk.gui.RowLayout(
+        slots=[
+            fileupload_component,
+            query_component,
+            button,
+        ]
+    ),
     id="gpt-index",
 )
 interface.launch()
