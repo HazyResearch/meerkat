76df11ec26108b0edc5071ec3277b95f631084c0
dilan-js <dilannana.js@gmail.com>
2023-01-28 18:55:16 -0800

    styling code cell

--- meerkat/interactive/app/src/lib/component/code_cell/CodeCell.svelte
+++ meerkat/interactive/app/src/lib/component/code_cell/CodeCell.svelte
@@ -38,10 +38,29 @@
 		// @ts-ignore
 		monaco = await import('monaco-editor');
 
+		
+
+		monaco.editor.defineTheme('myTheme', {
+		base: 'vs',
+		inherit: true,
+		rules: [{ background: 'EDF9FA', token:'' }],
+		colors: {
+			'editor.foreground': '#000000',
+			'editor.background': '#EDF9FA',
+			'editorCursor.foreground': '#8B0000',
+			'editor.lineHighlightBackground': '#0000FF20',
+			'editorLineNumber.foreground': '#008800',
+			'editor.selectionBackground': '#88000030',
+			'editor.inactiveSelectionBackground': '#88000015'
+		}
+		});
+		monaco.editor.setTheme('myTheme');
+
 		editor = monaco.editor.create(divEl, {
 			value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
 			language: 'python'
 		});
+
 		editor.addCommand(monaco.KeyMod.Shift | monaco.KeyCode.Enter, () => {
 			run_search(editor.getValue());
 		});
@@ -49,9 +68,17 @@
 			editor.dispose();
 		};
 	});
+
+
 </script>
 
-<div bind:this={divEl} class="h-32" />
+<div bind:this={divEl} class=" h-32" />
+<div class="text-right">
+	<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+		Button
+	  </button>
+</div>
+
 
 {#if error}
 	<div><h1>{error}</h1></div>
