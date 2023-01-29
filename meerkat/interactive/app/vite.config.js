ea08c1bbdfb03672ff321e2dbffe4d62d8cddf86
Karan Goel <kgoel93@gmail.com>
2023-01-28 20:26:11 -0800

    Add vitest

--- meerkat/interactive/app/vite.config.js
+++ meerkat/interactive/app/vite.config.js
@@ -17,24 +17,29 @@ const config = {
 		}
 	},
 
+	test: {
+		globals: true,
+		environment: 'jsdom',
+	},
+
 	// build: {
 	// 	lib: {
-			// entry: path.resolve(__dirname, 'src/lib/main.js'),
-			// name: 'Meerkat',
-			// fileName: 'meerkat'
-		// },
-		// rollupOptions: {
-		// 	// make sure to externalize deps that shouldn't be bundled
-		// 	// into your library
-		// 	external: ['svelte'],
-		// 	output: {
-		// 		// Provide global variables to use in the UMD build
-		// 		// for externalized deps
-		// 		globals: {
-		// 			svelte: 'Svelte'
-		// 		}
-		// 	},
-		// }
+	// entry: path.resolve(__dirname, 'src/lib/main.js'),
+	// name: 'Meerkat',
+	// fileName: 'meerkat'
+	// },
+	// rollupOptions: {
+	// 	// make sure to externalize deps that shouldn't be bundled
+	// 	// into your library
+	// 	external: ['svelte'],
+	// 	output: {
+	// 		// Provide global variables to use in the UMD build
+	// 		// for externalized deps
+	// 		globals: {
+	// 			svelte: 'Svelte'
+	// 		}
+	// 	},
+	// }
 	// }
 };
 
