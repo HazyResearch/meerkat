2801ebbe7e4c106357c04ba9c6c3a1a8c282e64c
Karan Goel <kgoel93@gmail.com>
2023-01-30 13:22:37 -0800

    Update GPT Index app

--- demo/gpt-index.py
+++ demo/gpt-index.py
@@ -5,7 +5,6 @@ from gpt_index import GPTSimpleVectorIndex, SimpleDirectoryReader
 
 import meerkat as mk
 from meerkat.interactive import Store
-from meerkat.interactive.app.src.lib.component.fileupload import FileUpload
 
 # Makes prints reactive!
 print = mk.gui.react()(rich.print)
@@ -94,7 +93,7 @@ print("\n", "Query:", query, "\n", "Dir:", dir, "\n", "Index:", index, "\n")
 
 page = mk.gui.Page(
     # Layout the Interface components one row each, and launch the page.
-    component=mk.gui.RowLayout(
+    component=mk.gui.html.flexcol(
         slots=[
             # fileupload_component,
             dir_component,
