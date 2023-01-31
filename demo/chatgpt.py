a2e7e84cf3b058643e3debf95be870a7aa5e9d1a
Arjun Desai <arjunaashna@gmail.com>
2023-01-30 18:50:00 -0800

    update chat

--- demo/chatgpt.py
+++ demo/chatgpt.py
@@ -10,6 +10,7 @@ Run:
     CHATGPT_SESSION_KEY="your_session_key" python chatgpt.py
 """
 import os
+from datetime import datetime
 
 from manifest import Manifest
 
@@ -21,32 +22,59 @@ manifest = Manifest(
     client_connection=os.environ.get("CHATGPT_SESSION_KEY"),
 )
 
-df = mk.DataFrame(
-    {
-        "message": ["hello. starting chat gpt"],
-        "sender": ["chatbot"],
-        "name": ["ChatBot"],
-        "time": ["2 hours ago"],
-    }
-)
+
+def get_time_elapsed(time):
+    # datetime.now() will be different for each cell, which may cause
+    # inconsistencies in the time elapsed column.
+    # TODO: Process the entire column at once.
+    # Probably wont be that noticeable on a minute scale.
+    time_elapsed = datetime.now() - time
+
+    # Days
+    days = time_elapsed.days
+    if days > 0:
+        return "{} day{} ago".format(days, "s" if days > 1 else "")
+
+    # Minutes
+    seconds = time_elapsed.seconds
+    if seconds > 60:
+        minutes = round(seconds / 60)
+        return "{} minute{} ago".format(minutes, "s" if minutes > 1 else "")
+
+    # Seconds
+    return "{} second{} ago".format(seconds, "s" if seconds > 1 else "")
+
+
+def _empty_df():
+    df = mk.DataFrame(
+        {
+            "message": ["Hi! I'm a chatbot."],
+            "sender": ["chatbot"],
+            "name": ["ChatBot"],
+            "send_time": [datetime.now()],
+        }
+    )
+    df["time"] = mk.defer(df["send_time"], get_time_elapsed)
+    return df
 
 
 @mk.gui.endpoint
 def on_send(df: mk.DataFrame, message: str):
+    message_time = datetime.now()
     chatbot = manifest.run(message)
-    df.set(
-        df.append(
-            mk.DataFrame(
-                {
-                    "message": [message, chatbot],
-                    "sender": ["user", "chatbot"],
-                    "name": ["Karan", "ChatBot"],
-                    "time": ["1 hour ago", "1 hour ago"],
-                }
-            )
-        )
+    new_df = mk.DataFrame(
+        {
+            "message": [message, chatbot],
+            "sender": ["user", "chatbot"],
+            "name": ["Karan", "ChatBot"],
+            "send_time": [message_time, datetime.now()],
+        }
     )
+    new_df["time"] = mk.defer(new_df["send_time"], get_time_elapsed)
+    df.set(df.append(new_df))
 
+
+df = _empty_df()
 
 chat = Chat(
     df=df,
