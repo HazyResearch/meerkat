e48cbeded39ae259e0ec027028de5859b094df2e
Arjun Desai <arjunaashna@gmail.com>
2023-01-30 18:30:22 -0800

    update collect env

--- meerkat/tools/collect_env.py
+++ meerkat/tools/collect_env.py
@@ -16,8 +16,8 @@ from tabulate import tabulate
 
 from meerkat.tools.lazy_loader import LazyLoader
 
-torch = LazyLoader('torch')
-torchvision = LazyLoader('torchvision')
+torch = LazyLoader("torch")
+torchvision = LazyLoader("torchvision")
 
 __all__ = ["collect_env_info"]
 
@@ -55,64 +55,11 @@ def detect_compute_compatibility(CUDA_HOME, so_file):
         return so_file
 
 
-def collect_env_info():
+def collect_torchvision_env():
+    from torch.utils.cpp_extension import CUDA_HOME
+
     has_cuda = torch.cuda.is_available()
-
     data = []
-    data.append(("sys.platform", sys.platform))
-    data.append(("platform.platform", platform.platform()))
-    data.append(
-        (
-            "node",
-            subprocess.run(["node", "--version"], stdout=subprocess.PIPE).stdout.decode(
-                "utf-8"
-            ),
-        )
-    )
-    data.append(
-        (
-            "npm",
-            subprocess.run(["npm", "--version"], stdout=subprocess.PIPE).stdout.decode(
-                "utf-8"
-            ),
-        )
-    )
-    data.append(("Python", sys.version.replace("\n", "")))
-    data.append(("numpy", np.__version__))
-
-    data.append(("PyTorch", torch.__version__ + " @" + os.path.dirname(torch.__file__)))
-    data.append(("PyTorch debug build", torch.version.debug))
-
-    data.append(("CUDA available", has_cuda))
-    if has_cuda:
-        devices = defaultdict(list)
-        for k in range(torch.cuda.device_count()):
-            devices[torch.cuda.get_device_name(k)].append(str(k))
-        for name, devids in devices.items():
-            data.append(("GPU " + ",".join(devids), name))
-
-        # NOTE: the use of CUDA_HOME requires the CUDA build deps, though in
-        # theory meddlr should be made runnable with only the CUDA runtime
-        from torch.utils.cpp_extension import CUDA_HOME
-
-        data.append(("CUDA_HOME", str(CUDA_HOME)))
-
-        if CUDA_HOME is not None and os.path.isdir(CUDA_HOME):
-            try:
-                nvcc = os.path.join(CUDA_HOME, "bin", "nvcc")
-                nvcc = subprocess.check_output(
-                    "'{}' -V | tail -n1".format(nvcc), shell=True
-                )
-                nvcc = nvcc.decode("utf-8").strip()
-            except subprocess.SubprocessError:
-                nvcc = "Not Available"
-            data.append(("NVCC", nvcc))
-
-        cuda_arch_list = os.environ.get("TORCH_CUDA_ARCH_LIST", None)
-        if cuda_arch_list:
-            data.append(("TORCH_CUDA_ARCH_LIST", cuda_arch_list))
-    data.append(("Pillow", PIL.__version__))
-
     try:
         import torchvision
 
@@ -133,9 +80,63 @@ def collect_env_info():
                 data.append(("torchvision._C", "failed to find"))
     except (AttributeError, ModuleNotFoundError):
         data.append(("torchvision", "unknown"))
+    return data
 
-    # Slurm info
-    data.append(("SLURM_JOB_ID", os.environ.get("SLURM_JOB_ID", "slurm not detected")))
+
+def _get_version(module_name, raise_error=False):
+    try:
+        return subprocess.run(
+            [module_name, "--version"], stdout=subprocess.PIPE
+        ).stdout.decode("utf-8")
+    except Exception as e:
+        if raise_error:
+            raise e
+        return "unknown"
+
+
+def collect_env_info():
+    has_cuda = torch.cuda.is_available()
+
+    data = []
+    data.append(("sys.platform", sys.platform))
+    data.append(("platform.platform", platform.platform()))
+    data.append(("node", _get_version("node")))
+    data.append(("node", _get_version("npm")))
+    data.append(("Python", sys.version.replace("\n", "")))
+    data.append(("numpy", np.__version__))
+
+    data.append(("PyTorch", torch.__version__ + " @" + os.path.dirname(torch.__file__)))
+    data.append(("PyTorch debug build", torch.version.debug))
+
+    data.append(("CUDA available", has_cuda))
+    if has_cuda:
+        devices = defaultdict(list)
+        for k in range(torch.cuda.device_count()):
+            devices[torch.cuda.get_device_name(k)].append(str(k))
+        for name, devids in devices.items():
+            data.append(("GPU " + ",".join(devids), name))
+        from torch.utils.cpp_extension import CUDA_HOME
+
+        data.append(("CUDA_HOME", str(CUDA_HOME)))
+
+        if CUDA_HOME is not None and os.path.isdir(CUDA_HOME):
+            try:
+                nvcc = os.path.join(CUDA_HOME, "bin", "nvcc")
+                nvcc = subprocess.check_output(
+                    "'{}' -V | tail -n1".format(nvcc), shell=True
+                )
+                nvcc = nvcc.decode("utf-8").strip()
+            except subprocess.SubprocessError:
+                nvcc = "Not Available"
+            data.append(("NVCC", nvcc))
+
+        cuda_arch_list = os.environ.get("TORCH_CUDA_ARCH_LIST", None)
+        if cuda_arch_list:
+            data.append(("TORCH_CUDA_ARCH_LIST", cuda_arch_list))
+    data.append(("Pillow", PIL.__version__))
+
+    # torchvision
+    data.extend(collect_torchvision_env())
 
     try:
         import cv2
@@ -143,6 +144,8 @@ def collect_env_info():
         data.append(("cv2", cv2.__version__))
     except ImportError:
         pass
+    # Slurm info
+    data.append(("SLURM_JOB_ID", os.environ.get("SLURM_JOB_ID", "slurm not detected")))
     env_str = tabulate(data) + "\n"
     env_str += collect_torch_env()
     return env_str
