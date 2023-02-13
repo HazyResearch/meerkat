6a4d11e672fd986f6f49975ecb95412412885591
Karan Goel <kgoel93@gmail.com>
2023-02-13 14:00:09 -0800

    Update filter

--- demo/filter.py
+++ demo/filter.py
@@ -2,9 +2,8 @@ import meerkat as mk
 
 df = mk.get("imagenette", version="160px")
 
-with mk.gui.react():
-    filter = mk.gui.Filter(df=df)
-    df = filter(df)
+filter = mk.gui.Filter(df)
+df = filter(df)
 
 # Gallery
 gallery = mk.gui.Gallery(
@@ -12,7 +11,6 @@ gallery = mk.gui.Gallery(
     main_column="img",
 )
 
-mk.gui.start(shareable=False)
 interface = mk.gui.Page(
     component=mk.gui.html.flexcol(slots=[filter, gallery]),
     id="filter",
