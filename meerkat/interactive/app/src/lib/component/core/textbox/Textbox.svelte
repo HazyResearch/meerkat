205f4dee917d900fdc86554ebdecea4734266c40
Karan Goel <kgoel93@gmail.com>
2023-02-21 20:12:42 -0800

    Update textbox

--- meerkat/interactive/app/src/lib/component/core/textbox/Textbox.svelte
+++ meerkat/interactive/app/src/lib/component/core/textbox/Textbox.svelte
@@ -2,18 +2,24 @@
 	/** Text in the textbox. */
 	export let text: string;
 
+	/** Placeholder text. */
+	export let placeholder: string = "Write some text...";
+
+	/** Debounce timer (in ms). */
+	export let debounceTimer: number = 150;
+
 	let timer: any;
 	const debounce = (v: string) => {
 		clearTimeout(timer);
 		timer = setTimeout(() => {
 			text = v;
-		}, 150);
+		}, debounceTimer);
 	};
 </script>
 
 <input
 	type="text"
-	placeholder="Write some text..."
+	{placeholder}
 	class="grow h-10 px-3 rounded-md shadow-md my-1 border-gray-400"
 	on:keyup={({ target: { value } }) => debounce(value)}
 	on:keypress
