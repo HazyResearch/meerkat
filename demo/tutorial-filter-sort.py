bca3acb6b2a331c42be26e7ed05e88081d957ce7
Dean Stratakos <dastratakos@gmail.com>
2023-02-23 18:13:28 -0800

    Push tutorial-0

---
+++ demo/tutorial-filter-sort.py
@@ -0,0 +1,18 @@
+import meerkat as mk
+
+df = mk.get("imagenette", version="160px")
+
+filter = mk.gui.Filter(df=df)
+df = filter(df)
+
+criteria = mk.gui.Store([])
+sort = mk.gui.Sort(df=df, criteria=criteria)
+df = sort(df)
+
+gallery = mk.gui.Gallery(df=df, main_column="img")
+
+mk.gui.start()
+page = mk.gui.Page(
+    component=mk.gui.RowLayout(slots=[filter, sort, gallery]), id="filter-sort"
+)
+page.launch()
