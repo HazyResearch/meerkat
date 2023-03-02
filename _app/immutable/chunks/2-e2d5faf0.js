8de2995ca1cfd9e2fcb5d56516ad0e2f94c8913b
seyuboglu <seyuboglu@users.noreply.github.com>
2023-03-02 17:39:29 +0000

    deploy: bbe5cbf501650d494c6faee2567082710d2269aa

--- _app/immutable/chunks/2-e2d5faf0.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-229a273e.js";export{t as component};
---
+++ _app/immutable/chunks/2-f78350e1.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-cd81b4f4.js";export{t as component};
