f4d99b50011d26dbca7c613b972518729f1a86e0
Arjun Desai <arjunaashna@gmail.com>
2023-03-28 14:53:35 -0700

    extract

--- meerkat/ops/clever/extract.py
+++ meerkat/ops/clever/extract.py
@@ -1,73 +1,106 @@
 from meerkat.columns.scalar import ScalarColumn
 from meerkat.dataframe import DataFrame
-from meerkat.ops.clever.chat import chat
+from meerkat.engines.abstract import TextCompletion
+from meerkat.row import Row
 
-from .impute import _prepare_row
 
-_TRAIN_MASK_COLUMN = "__train_mask__"
+def _prepare_row(row: Row, output_column: str, is_test: bool):
+    CELL_FORMAT = "{name}: {value}\n"
+
+    row_str = "---begin row---\n"
+
+    for name, value in row.items():
+        if name == output_column:
+            continue
+        row_str += CELL_FORMAT.format(name=name, value=value)
+
+    if not is_test:
+        row_str += CELL_FORMAT.format(name=output_column, value=row[output_column])
+    else:
+        row_str += CELL_FORMAT.format(name=output_column, value="").rstrip()
+
+    return row_str
 
 
 def extract(
     df: DataFrame,
-    column: str = None,
-    missing: str = "nan",
-    train_mask: ScalarColumn = None,
+    *,
+    annotated_df: DataFrame,
+    column: str,
+    to: str,
+    engine: TextCompletion = None,
+    schema: str = None
 ):
-    """Extract information from the dataframe.
-
-    Given some data (i.e. ``df``), extract out information about the column.
+    """Extract information given some annotated examples.
 
     Args:
-        df: The dataframe.
-        missing: The value to use for finding missing entries in ``df[column]``.
-            This will be ignored if ``train_mask`` is not None.
-        train_mask: The mask to use for in-context examples in the model.
-            If None, then all rows in ``df`` that are not missing will be used.
+        df: The dataframe with values to extract.
+        annotated_df: The dataframe with few-shot examples.
+            This dataframe should contain both columns specified in
+            ``from`` and ``to``.
+        column: The column to extract information from.
+        to: The column to extract information to.
+        engine: The engine to use for extracting information.
+        schema: The schema to validate the extraction against.
+            This schema will not be used for prompting the language model.
+
+    Return:
+
     """
-    if not isinstance(df[column], ScalarColumn):
+    column_name = column
+    if not isinstance(df[to], ScalarColumn):
         raise TypeError("Extraction is only supported for scalar columns.")
 
-    column_name = column
-    column: ScalarColumn = df[column]
+    # Annotated examples.
+    annot_input = annotated_df[[column_name, to]]
 
-    if train_mask is None:
-        if missing == "nan":
-            missing_mask = column.isna()
-        else:
-            missing_mask = column == missing
-        train_df = df[~missing_mask]
-    else:
-        train_df = df[train_mask]
-
-    if len(train_df) == 0:
-        raise ValueError(
-            "Could not find any training examples. "
-            "Please specify ``train_mask`` with at least one non-zero entry."
-        )
-
-    # Prepare the in-context examples.
+    # Prepare the in-context training examples.
     in_ctx_examples = "\n".join(
-        train_df.head().map(
-            lambda row: _prepare_row(row, column=column_name, missing=False),
+        annot_input.map(
+            lambda row: _prepare_row(row, output_column=to, is_test=False),
             inputs="row",
         )
     )
 
-    # Make a new dataframe where we append the train mask.
-    df = df.copy()
-    df[_TRAIN_MASK_COLUMN] = train_mask
-
-    return df.map(
-        _impute.partial(column_name=column_name, in_ctx_examples=in_ctx_examples),
+    # Run the LLM
+    test_df = df[[column]]
+    return test_df.map(
+        lambda row: engine.run(
+            prompt=in_ctx_examples + "\n" + _prepare_row(row, column=to, is_test=True)
+        ),
         inputs="row",
     )
 
 
-def _impute(row, column_name: str, in_ctx_examples: str):
-    """Impute a row."""
-    if row[_TRAIN_MASK_COLUMN]:
-        return row[column_name]
+def extract_to_schema(
+    column: ScalarColumn,
+    schema: str,
+    engine: TextCompletion = None,
+) -> ScalarColumn:
+    """Extract information from a column to a specified schema.
 
-    return chat(
-        in_ctx_examples + "\n" + _prepare_row(row, column=column_name, missing=True)
-    )[0]
+    Args:
+        column: The column to extract information from.
+        schema: The schema to tell the LLM to format the extraction into.
+        engine: The engine to use for extracting information.
+
+    Return:
+        A column with the extracted information.
+    """
+    prompt = """
+    Extract information from the TEXT to a SCHEMA.
+
+    TEXT: {text}
+    SCHEMA: {schema}
+    """
+
+    return column.map(
+        lambda text: engine.run(prompt=prompt.format(text=text, schema=schema))
+    )
+
+
+def _run_llm(row, to: str, engine: TextCompletion, in_ctx_examples: str):
+    """Run extraction for a single test row."""
+    return engine.run(
+        prompt=in_ctx_examples + "\n" + _prepare_row(row, column=to, is_test=True)
+    )
