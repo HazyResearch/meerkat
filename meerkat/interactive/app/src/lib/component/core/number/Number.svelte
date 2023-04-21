3909547de083ba892b750ca7e4dfecb9e8b6aff9
Dean Stratakos <dastratakos@gmail.com>
2023-04-20 17:02:11 -0700

    Add Number editing functionality

--- meerkat/interactive/app/src/lib/component/core/number/Number.svelte
+++ meerkat/interactive/app/src/lib/component/core/number/Number.svelte
@@ -1,36 +1,58 @@
 <script lang="ts">
+	import { getContext } from 'svelte/internal';
+
 	export let data: any;
-	export let dtype: string = "auto";
-    export let precision: number = 3
-    export let percentage: boolean = false
-    export let classes: string = ""
+	export let dtype: string = 'auto';
+	export let precision: number = 3;
+	export let percentage: boolean = false;
+	export let editable: boolean = false;
+	export let classes: string = '';
 
-    if(dtype === "auto"){
-        if(typeof data === "number") {
-            
-            if(Number.isInteger(data)) {
-                dtype = "int";
-            } else {
-                dtype = "float";
-            }
-        } else {
-            dtype = "string"
-        }
-    }
-    
-    
-    if (dtype === "float") {
-        if (percentage) {
-            data = (data * 100).toPrecision(precision) + "%";
-        } else if (!isNaN(data)) {
-            data = data.toPrecision(precision);
-        } else {
-            data = "NaN";
-        }
-    }
+	const cellEdit: CallableFunction = getContext('cellEdit');
 
+	let prevData: any = data;
+	let dtypeBefore: string = dtype;
 
+	if (dtype === 'auto') {
+		// The + operator converts a string or number to a number if possible
+		// if (typeof +data === 'number') {
+		if (typeof data === 'number') {
+			// data = +data;
+			if (Number.isInteger(data)) {
+				dtype = 'int';
+			} else {
+				dtype = 'float';
+			}
+		} else {
+			dtype = 'string';
+		}
+	}
+
+	if (dtype === 'float') {
+		if (typeof data === 'string') {
+			data = +data;
+		}
+		if (percentage) {
+			data = (data * 100).toPrecision(precision) + '%';
+		} else if (!isNaN(data)) {
+			console.log(data, typeof data, dtypeBefore);
+			data = data.toPrecision(precision).toString();
+		} else {
+			data = 'NaN';
+		}
+	}
 </script>
-<div class={classes}>
-    {data}
-</div>
+
+{#if editable}
+	<input
+		class="input w-full"
+		on:change={() => {
+			cellEdit(dtype === 'string' ? data : +data);
+		}}
+		bind:value={data}
+	/>
+{:else}
+	<div class={classes}>
+		{data}
+	</div>
+{/if}
