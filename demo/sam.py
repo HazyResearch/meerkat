c0bcf984d5735a304d68e866ed793c9d8a323823
Arjun Desai <ad12@users.noreply.github.com>
2023-04-25 08:21:30 -0700

    add points to sam demo (#330)

--- demo/sam.py
+++ demo/sam.py
@@ -83,6 +83,66 @@ def predict(image, boxes: mk.Store[List], segmentations: mk.Store[List]):
 
 
 @mk.reactive()
+def predict_point(
+    image, points: mk.Store[List], segmentations: mk.Store[List], selected_category: str
+):
+    """Segment the image with the SAM model.
+
+    When ``boxes`` changes, this function will be called to segment the image.
+    The segmentation will be appended to the segmentations list.
+    This is a bit hacky, but it should work.
+
+    Note:
+        Because this will run any time ``boxes`` changes, it will also run
+        when boxes are deleted, which can lead to unexpected behavior.
+
+    Returns:
+        np.ndarray: The colorized segmentation mask.
+    """
+    image = prepare_image(image)
+
+    predictor.set_image(np.asarray(image))
+
+    combined_segmentations = []
+    if len(points) > 0:
+        points = np.asarray([[point["x"], point["y"]] for point in points])
+        # Assume that all points are foreground points.
+        point_labels = np.ones(len(points))
+        masks, scores, _ = predictor.predict(
+            point_coords=points, point_labels=point_labels
+        )
+        best_idx = np.argsort(scores)[-1]
+
+        # If there is already a prediction for this category,
+        # we assume that we are operating on this prediction.
+        # Remove this mask and add the new one.
+        # TODO: Use this mask as a prompt for the prediction.
+        categories = [seg[1] for seg in segmentations]
+        if selected_category in categories:
+            segmentations[categories.index(selected_category)] = (
+                masks[best_idx],
+                selected_category,
+            )
+        else:
+            predictions = [(masks[best_idx], selected_category)]
+            segmentations.extend(predictions)
+    combined_segmentations.extend(segmentations)
+
+    # Do this to trigger the reactive function.
+    # segmentations.set(segmentations)
+    return mk.Store(combined_segmentations, backend_only=True)
+
+
+@mk.reactive()
+def clear_points(points: List, selected_category: str):
+    """When the selected category changes, clear the points."""
+    # We do not want to trigger segmentation to re-run here.
+    # So we use a hacky solution and clear the points.
+    points.clear()
+    return points
+
+
+@mk.reactive()
 def get_categories(segmentations: List[str]):
     return [seg[1] for seg in segmentations]
 
@@ -131,8 +191,8 @@ def update_annotations(idx: int, ann_type: str, annotations: List[str]):
 # Build the dataframe.
 files = [
     # "https://kulapartners.com/wp-content/uploads/2017/06/multiple-personas-hero.jpg",
-    # "https://3acf3052-cdn.agilitycms.cloud/images/service/KNEE%20SAGITTAL.jpg",
-    "https://www.mercurynews.com/wp-content/uploads/2022/01/BNG-L-WARRIORS-0122-28.jpg?w=1024"
+    "https://3acf3052-cdn.agilitycms.cloud/images/service/KNEE%20SAGITTAL.jpg",
+    # "https://www.mercurynews.com/wp-content/uploads/2022/01/BNG-L-WARRIORS-0122-28.jpg?w=1024"
 ]
 images = mk.DataFrame({"image": mk.files(files, type="image")})
 # Add an empty list for each of the annotations.
@@ -143,18 +203,29 @@ images["points"] = [[] for _ in range(len(images))]
 
 
 idx = mk.Store(0, backend_only=True)
+selected_category = mk.Store("")
 img, segmentations, boxes, points = get_img_and_annotations(idx)
 update_annotations(idx, "boxes", boxes)
 update_annotations(idx, "points", points)
+points = clear_points(points=points.unmark(), selected_category=selected_category)
 
-combined_segmentations = predict(
+box_segmentations = predict(
     image=img,
     boxes=boxes,
     segmentations=segmentations,
 )
+point_segmentations = predict_point(
+    image=img,
+    points=points,
+    segmentations=segmentations,
+    selected_category=selected_category.unmark(),
+)
+
+with mk.magic():
+    combined_segmentations = box_segmentations + point_segmentations
 
 # Image annotator.
-selected_category = mk.Store("")
+selected_category.mark()
 categories = get_categories(segmentations)
 annotator = mk.gui.ImageAnnotator(
     img,
