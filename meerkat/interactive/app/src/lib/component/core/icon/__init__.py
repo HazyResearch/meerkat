df58519d35dac51d6f42da432c143124870cf94a
Arjun Desai <arjunaashna@gmail.com>
2023-04-28 05:58:10 -0700

    add more icons

--- meerkat/interactive/app/src/lib/component/core/icon/__init__.py
+++ meerkat/interactive/app/src/lib/component/core/icon/__init__.py
@@ -1,6 +1,10 @@
+from typing import Optional
+
 from meerkat.interactive.app.src.lib.component.abstract import Component
 
 
 class Icon(Component):
     data: str = ""
     name: str = "Globe2"
+    fill: Optional[str] = None
+    size: int = 16
