ccfdfc1d3a552d6403de4620f8cdeabc2994e96e
ad12 <ad12@users.noreply.github.com>
2023-04-30 14:49:45 +0000

    deploy: 7d4d7546ec5565f92c7fe7ebfd01f07555df7b2b

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.10a1',
+    VERSION: '0.4.10a2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
