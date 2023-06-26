b9333886518364dab039276e1368be190c87961d
Arjun Desai <ad12@users.noreply.github.com>
2023-06-26 07:38:25 -0700

    add zoom and pan to image (#369)

--- meerkat/interactive/app/src/lib/component/core/image/Image.svelte
+++ meerkat/interactive/app/src/lib/component/core/image/Image.svelte
@@ -1,6 +1,61 @@
 <script lang="ts">
+	// Encoded data.
 	export let data: string;
+	// Alt text.
+	export let alt: string = '';
 	export let classes: string = '';
+	export let style = '';
+
+	// Transform parameters.
+	export let enableZoom: boolean = false;
+	export let enablePan: boolean = false;
+	export let zoom: number = 1.0;
+	export let offset: Array<number> = [0, 0];
+	
+	// Cursor style.
+	export let cursor: string = 'default';
+
+	console.log("Image.svelte: enableZoom = ", zoom);
+
+	const handleWheel = (event: WheelEvent) => {
+		const { deltaY } = event;
+		event.preventDefault();
+		zoom += deltaY * 0.01;
+		zoom = Math.max(zoom, 1);
+	};
+
+	const handleMouseDown = (event) => {
+		event.preventDefault();
+		const { clientX, clientY } = event;
+		let lastX = clientX;
+		let lastY = clientY;
+		cursor = 'grabbing';
+
+		const handleMouseMove = (event) => {
+			const { clientX, clientY } = event;
+			offset[0] += clientX - lastX;
+			offset[1] += clientY - lastY;
+			lastX = clientX;
+			lastY = clientY;
+		};
+
+		const handleMouseUp = () => {
+			window.removeEventListener('mousemove', handleMouseMove);
+			window.removeEventListener('mouseup', handleMouseUp);
+			cursor = 'default';
+		};
+
+		window.addEventListener('mousemove', handleMouseMove);
+		window.addEventListener('mouseup', handleMouseUp);
+	};
+
 </script>
 
-<img class={classes} src={data} alt="A Meerkat cell." />
+<img
+	class={classes}
+	src={data}
+	{alt}
+	style="cursor:{cursor}; transform: translate({offset[0]}px, {offset[1]}px) scale({zoom}); {style}"
+	on:wheel={(e) => {enableZoom && handleWheel(e)}}
+	on:mousedown={(e) => {enablePan && handleMouseDown(e)}}
+/>
--- meerkat/interactive/app/src/lib/component/core/image/__init__.py
+++ meerkat/interactive/app/src/lib/component/core/image/__init__.py
@@ -4,3 +4,5 @@ from meerkat.interactive.app.src.lib.component.abstract import Component
 class Image(Component):
     data: str
     classes: str = ""
+    enable_zoom: bool = False
+    enable_pan: bool = False
